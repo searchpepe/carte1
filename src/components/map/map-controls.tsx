536d0600b314162583a2e74b4369431dece26d29
Azur <153006577+searchpepe@users.noreply.github.com>
2026-01-09 19:27:51 +0100

    Change map controls position to top

--- src/components/map/map-controls.tsx
+++ src/components/map/map-controls.tsx
@@ -16,7 +16,7 @@ export default function MapCotrols() {
   };
 
   return (
-    <aside className="absolute bottom-8 right-4 z-10 bg-background p-2 rounded-lg shadow-lg flex flex-col gap-2">
+    <aside className="absolute top-36 right-4 z-10 bg-background p-2 rounded-lg shadow-lg flex flex-col gap-2">
       <Button variant="ghost" size="icon" onClick={zoomIn}>
         <PlusIcon className="w-5 h-5" />
         <span className="sr-only">Zoom in</span>
